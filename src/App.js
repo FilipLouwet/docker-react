31b2ef593c56f0e3f703bb973af423d17570d115
Filip Louwet <filiplouwet@gmail.com>
2020-05-27 22:39:27 +0200

    added travis file

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I have a lovely dsfdsf of coconuts!
+          I have a lovely bunch of coconuts!
         </p>
         <a
           className="App-link"
